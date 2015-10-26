f1f7261479b343dc8a6d85be4a0c456b06766f6c
Matheus Souza <matheusviegasdesouza@gmail.com>
2015-10-25 23:04:57 -0200

    Delete config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define('DB_HOSTNAME', 'servidor'); // Servidor (Geralmente é localhost)
-define('DB_USERNAME', 'usuario'); // Usuario do Banco
-define('DB_PASSWORD', 'senha'); // Senha
-define('DB_DATABASE', 'banco'); // Nome do Banco
-define('DB_PREFIX', 'prefixo'); // Prefixo das Tabelas
-define('DB_CHARSET', 'utf8'); // Codificação (Não Alterar)
