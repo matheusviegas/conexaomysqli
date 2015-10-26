4800a7ca40638ff3fe317363b1964e4dceda921e
Matheus Souza <matheusviegasdesouza@gmail.com>
2015-10-26 08:59:15 -0200

    Arquivo de Configuração

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 
-define('DB_HOSTNAME', 'servidor'); // Servidor (Geralmente é localhost)
-define('DB_USERNAME', 'usuario'); // Usuario do Banco
+define('DB_HOSTNAME', 'localhost'); // Servidor (Geralmente é localhost)
+define('DB_USERNAME', 'root'); // Usuario do Banco
 define('DB_PASSWORD', 'senha'); // Senha
-define('DB_DATABASE', 'banco'); // Nome do Banco
-define('DB_PREFIX', 'prefixo'); // Prefixo das Tabelas
+define('DB_DATABASE', 'nomeBanco'); // Nome do Banco
+define('DB_PREFIX', 'prefixo'); // Prefixo das Tabelas (Se não tiver prefixo, deixe null)
 define('DB_CHARSET', 'utf8'); // Codificação (Não Alterar)
