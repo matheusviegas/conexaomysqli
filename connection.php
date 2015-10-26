8b73477bcb4f4d18835aae8aebbdd7e320366dd2
Matheus Souza <matheusviegasdesouza@gmail.com>
2015-10-26 09:00:14 -0200

    Conexão com o Banco de Dados

--- connection.php
+++ connection.php
@@ -1,8 +1,13 @@
 <?php
 
+	// Autor: Matheus Souza
+	// Repositório: https://github.com/matheusviegas/conexaomysqli
+	// Página Oficial do Projeto: http://matheusviegas.github.io/conexaomysqli/
+
+
 	// Protege contra SQL Injection
-	function DBEscape($data){
-		$link = DBConnect();
+	function escape($data){
+		$link = AbrirConexao();
 		
 		if(!is_array($data))
 			$dados = mysqli_real_escape_string($link, $data);
@@ -17,17 +22,17 @@
 			}
 		}
 		
-		DBClose($link);
+		FecharConexao($link);
 		return $data;
 	}
 
 	// Fecha Conexão com MySQL
-	function DBClose($link){
+	function FecharConexao($link){
 		@mysqli_close($link) or die(mysqli_error($link));
 	}
 
 	// Abre com Conexão com MySQL
-	function DBConnect(){
+	function AbrirConexao(){
 		$link = @mysqli_connect(DB_HOSTNAME, DB_USERNAME, DB_PASSWORD, DB_DATABASE) or die(mysqli_connect_error());
 		mysqli_set_charset($link, DB_CHARSET) or die(mysqli_error($link));
 		
